3dc5ab3d055ea97ec18af007f08e1e3d69559778
Roman <r-110>
2023-05-10 17:05:40 +0200

    actual version

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"regexp"
 	"strconv"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -25,14 +28,25 @@ func dataAccumulation(text string, total *int) {
 	*total += number
 }
 
-func main() {
+func connectToDB() *dynamodb.Client {
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-central-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Using the Config value, create the DynamoDB client
+	return dynamodb.NewFromConfig(cfg)
+}
+
+func connectToTelegramBot() *tgbotapi.BotAPI {
 	bot, err := tgbotapi.NewBotAPI("5505578325:AAE4sHnqPUc-VYi9rYQpuJsj4VPxAP_7_Uc")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	wh, _ := tgbotapi.NewWebhook("https://3c7f-93-95-139-165.ngrok-free.app/" + bot.Token)
@@ -50,11 +64,16 @@ func main() {
 	if info.LastErrorDate != 0 {
 		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
 	}
+	return bot
+}
+func main() {
+	dbClient := connectToDB()
+	bot := connectToTelegramBot()
+
+	total := 0
 
 	updates := bot.ListenForWebhook("/" + bot.Token)
 	go http.ListenAndServe("0.0.0.0:8443", nil)
-
-	total := 0
 
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message updates
