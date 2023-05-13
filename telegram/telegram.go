66abf6d900f68ebda5d0103a9a70c631d1ae350d
Roman <r-110>
2023-05-13 15:02:08 +0200

    Added two folders

---
+++ telegram/telegram.go
@@ -0,0 +1,34 @@
+package telegram
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func ConnectToTelegramBot() *tgbotapi.BotAPI {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	wh, _ := tgbotapi.NewWebhook(os.Getenv("NGROK_URL") + bot.Token)
+
+	_, err = bot.Request(wh)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	return bot
+}
