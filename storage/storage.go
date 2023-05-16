99ccbe46a9038c88b05892b6a428b02141c17f45
djezzzl <lawliet.djez@gmail.com>
2023-05-16 15:45:05 +0200

    Add CalculateTotal function

--- storage/storage.go
+++ storage/storage.go
@@ -42,6 +42,35 @@ func (client *Client) Connect() {
 	println("Successfully connected to the storage")
 }
 
+func (client *Client) CalculateTotal() int {
+	collection := client.collection()
+	pipeline := []bson.M{
+		{
+			"$group": bson.M{
+				"_id":   nil,
+				"total": bson.M{"$sum": "$Value"},
+			},
+		},
+	}
+	cursor, err := collection.Aggregate(context.TODO(), pipeline)
+	if err != nil {
+		panic(err)
+	}
+
+	defer cursor.Close(context.TODO())
+
+	if cursor.Next(context.TODO()) {
+		var result bson.M
+		err = cursor.Decode(&result)
+		if err != nil {
+			panic(err)
+		}
+		return int(result["total"].(int32))
+	}
+
+	return 0
+}
+
 func (client *Client) Write(chatID int64, date int, value int) {
 	// Insert a single document
 	res, err := client.collection().InsertOne(context.TODO(), bson.M{
