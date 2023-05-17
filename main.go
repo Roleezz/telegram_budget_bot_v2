2474a3503eaaec4bdb29745f0a588d629ae1332a
Roman <r-110>
2023-05-17 10:52:30 +0200

    Changing the main/telegram package

--- main.go
+++ main.go
@@ -1,51 +1,9 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"regexp"
-	"strconv"
-
-	"github.com/Roleezz/telegram_budget_bot_v2/storage"
 	"github.com/Roleezz/telegram_budget_bot_v2/telegram"
 )
 
 func main() {
-	storageClient := new(storage.Client)
-	storageClient.Connect()
-
-	bot := telegram.ConnectToTelegramBot()
-	updates := bot.ListenForWebhook("/" + bot.Token)
-
-	go http.ListenAndServe("0.0.0.0:8443", nil)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message updates
-			continue
-		}
-
-		if update.Message.IsCommand() { // ignore any non-command Messages
-			// Extract the command from the Message.
-			switch update.Message.Command() {
-			case "stats":
-				total := storageClient.CalculateTotal()
-				telegram.WriteMessage(bot, update.Message.Chat.ID, strconv.Itoa(total))
-			}
-		} else {
-			re := regexp.MustCompile(`^\s*\d+\.?\d+`)
-			answer := re.FindAllString(update.Message.Text, 1)
-
-			if answer != nil {
-				number, err := strconv.Atoi(answer[0])
-
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				storageClient.Write(update.Message.Chat.ID, update.Message.Date, number)
-			} else {
-				telegram.WriteMessage(bot, update.Message.Chat.ID, "Not matched")
-			}
-		}
-	}
+	telegram.Update()
 }
--- telegram/telegram.go
+++ telegram/telegram.go
@@ -2,12 +2,14 @@ package telegram
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"regexp"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func WriteMessage(bot *tgbotapi.BotAPI, chatID int64, text string) {
+func writeMessage(bot *tgbotapi.BotAPI, chatID int64, text string) {
 	msg := tgbotapi.NewMessage(chatID, text)
 
 	if _, err := bot.Send(msg); err != nil {
@@ -15,7 +17,7 @@ func WriteMessage(bot *tgbotapi.BotAPI, chatID int64, text string) {
 	}
 }
 
-func ConnectToTelegramBot() *tgbotapi.BotAPI {
+func connectToTelegramBot() *tgbotapi.BotAPI {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
@@ -39,4 +41,44 @@ func ConnectToTelegramBot() *tgbotapi.BotAPI {
 		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
 	}
 	return bot
+}
+
+func Update() {
+	//storageClient := new(storage.Client)
+	//storageClient.Connect()
+	connect := connectToTelegramBot()
+	updates := connect.ListenForWebhook("/" + connect.Token)
+	go http.ListenAndServe("0.0.0.0:8443", nil)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message updates
+			continue
+		}
+
+		if update.Message.IsCommand() { // ignore any non-command Messages
+			// Extract the command from the Message.
+			switch update.Message.Command() {
+			case "stats":
+				// total := storageClient.CalculateTotal()
+				// strconv.Itoa(total)
+				writeMessage(connect, update.Message.Chat.ID, "55")
+			}
+		} else {
+			re := regexp.MustCompile(`^\s*\d+\.?\d+`)
+			answer := re.FindAllString(update.Message.Text, 1)
+
+			if answer != nil {
+				writeMessage(connect, update.Message.Chat.ID, "Nice")
+				// number, err := strconv.Atoi(answer[0])
+
+				// if err != nil {
+				// 	log.Fatal(err)
+				// }
+
+				// storageClient.Write(update.Message.Chat.ID, update.Message.Date, number)
+			} else {
+				writeMessage(connect, update.Message.Chat.ID, "Not matched")
+			}
+		}
+	}
 }
