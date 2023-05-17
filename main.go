7a7b1f1d78fc9aae66cf631effb2578ce1b666e0
Roman <r-110>
2023-05-17 12:32:21 +0200

    Changing the structure of main.go

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"github.com/Roleezz/telegram_budget_bot_v2/storage"
 	"github.com/Roleezz/telegram_budget_bot_v2/telegram"
 )
 
 func main() {
-	telegram.Update()
+	storageClient := new(storage.Client)
+	storageClient.Connect()
+
+	telegramClient := new(telegram.Client)
+	telegramClient.ConnectToTelegramBot()
+
+	telegramClient.Update(storageClient.CalculateTotal, storageClient.Write)
+
 }
